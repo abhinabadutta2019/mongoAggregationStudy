8f224f39a0c168a045f086d94869a4e050f6b13b
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-03-28 02:42:17 +0530

    corrected - connected and added all data to mongodb

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes/authRoutes");
+// const routes = require("./routes/authRoutes");
 
 const app = express();
 
 //middleware
 app.use(express.json());
 
-// database connection
-const dbURI = "mongodb://127.0.0.1:27017/Persons";
+////////////////////////////////////////
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("Persons.json", "utf8"));
+//////////////////////
 mongoose
-  .connect(dbURI, {
+  .connect("mongodb://127.0.0.1:27017/mongo-aggregation-practice", {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
-
-//routes
-app.use("/", routes);
-
-//
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/Persons", { useNewUrlParser: true })
-//   .then(() => {
-//     return mongoose.connection.dropDatabase();
-//   })
-//   .then(() => {
-//     //for loop to add each object of this array of object
-//     for (let i = 0; i < data.length; i++) {
-//       const person = data[i];
-//       //   console.log(person);
-//       //
-//       //adding new Date- before the date key/value-
-//       person.registered = new Date(person.registered);
-//       //insert data to database
-//       mongoose.connection.collection("16th-march").insertOne(person);
-//     }
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//     mongoose.connection.close();
-//   });
+  .then(() => {
+    return mongoose.connection.dropDatabase();
+  })
+  .then(() => {
+    //for loop to add each object of this array of object
+    for (let i = 0; i < data.length; i++) {
+      const person = data[i];
+      //   console.log(person);
+      //
+      //adding new Date- before the date key/value-
+      person.registered = new Date(person.registered);
+      //insert data to database
+      mongoose.connection.collection("persons").insertOne(person);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    mongoose.connection.close();
+  });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,4 +20,23 @@ router.get("/index-less-than-10", (req, res) => {
     });
 });
 
+//with filter
+//getting data while index <10 and sort them by index order
+router.get("/index-less-than-10", (req, res) => {
+  const collection = mongoose.connection.collection("16th-march");
+
+  collection
+    .find({ index: { $lt: 10 } })
+    .filter((doc) => doc.index < 10)
+    .sort((a, b) => a.index - b.index)
+    .toArray()
+    .then((docs) => {
+      console.log(docs);
+      res.send(docs);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send({ error: "Error fetching documents" });
+    });
+});
 module.exports = router;
