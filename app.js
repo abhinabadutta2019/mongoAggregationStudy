5faf83ea8794fae9a2904c529490cf3158136f6f
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-03-28 03:14:43 +0530

    created a route with async/await

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const routes = require("./routes/authRoutes");
+const routes = require("./routes/authRoutes");
 
 const app = express();
 
 //middleware
 app.use(express.json());
 
-////////////////////////////////////////
-const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("Persons.json", "utf8"));
-//////////////////////
+// database connection
+const dbURI = "mongodb://127.0.0.1:27017/mongo-aggregation-practice";
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mongo-aggregation-practice", {
+  .connect(dbURI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .then(() => {
-    return mongoose.connection.dropDatabase();
-  })
-  .then(() => {
-    //for loop to add each object of this array of object
-    for (let i = 0; i < data.length; i++) {
-      const person = data[i];
-      //   console.log(person);
-      //
-      //adding new Date- before the date key/value-
-      person.registered = new Date(person.registered);
-      //insert data to database
-      mongoose.connection.collection("persons").insertOne(person);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    mongoose.connection.close();
-  });
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes;
+app.use("/", routes);
+////////////////////////////////////////
+
+// const fs = require("fs");
+// const data = JSON.parse(fs.readFileSync("Persons.json", "utf8"));
+
+//adding the data to database
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/mongo-aggregation-practice", {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => {
+//     return mongoose.connection.dropDatabase();
+//   })
+//   .then(() => {
+//     //for loop to add each object of this array of object
+//     for (let i = 0; i < data.length; i++) {
+//       const person = data[i];
+//       //   console.log(person);
+//       //
+//       //adding new Date- before the date key/value-
+//       person.registered = new Date(person.registered);
+//       //insert data to database
+//       mongoose.connection.collection("persons").insertOne(person);
+//     }
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     mongoose.connection.close();
+//   });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,41 +2,22 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-//getting data while index <10 and sort them by index order
-router.get("/index-less-than-10", (req, res) => {
-  const collection = mongoose.connection.collection("16th-march");
+//getting data while index <5 and sort them by index order
+router.get("/index-less-than-5", async (req, res) => {
+  const collection = mongoose.connection.collection("persons");
 
-  collection
-    .find({ index: { $lt: 10 } })
-    .toArray()
-    .then((docs) => {
-      const sortedDocs = docs.sort((a, b) => a.index - b.index);
-      console.log(sortedDocs);
-      res.send(sortedDocs);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send({ error: "Error fetching documents" });
-    });
+  try {
+    const docs = await collection
+      .find({ index: { $lt: 5 } })
+      .sort({ index: -1 })
+      .toArray();
+    console.log(docs);
+    res.send(docs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: "Error fetching documents" });
+  }
 });
 
-//with filter
-//getting data while index <10 and sort them by index order
-router.get("/index-less-than-10", (req, res) => {
-  const collection = mongoose.connection.collection("16th-march");
-
-  collection
-    .find({ index: { $lt: 10 } })
-    .filter((doc) => doc.index < 10)
-    .sort((a, b) => a.index - b.index)
-    .toArray()
-    .then((docs) => {
-      console.log(docs);
-      res.send(docs);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send({ error: "Error fetching documents" });
-    });
-});
+//
 module.exports = router;
