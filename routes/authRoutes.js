58da048241b6e8c090bdbf356abdacdda696e4b9
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-03-28 04:36:46 +0530

    same route aggregate vs find

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -97,5 +97,25 @@ router.get("/female-age-gt-39", async (req, res) => {
     res.status(500).send({ error: "Error fetching documents" });
   }
 });
+
+// find -- female-age-gt-39-tags-size-3
+router.get("/female-age-gt-39-tags-size-3", async (req, res) => {
+  const collection = mongoose.connection.collection("persons");
+
+  try {
+    const docs = await collection
+      .find({
+        gender: "female",
+        age: { $gt: 38 },
+        tags: { $size: 3 },
+      })
+      .toArray();
+    console.log(docs);
+    res.send(docs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: "Error fetching documents" });
+  }
+});
 //
 module.exports = router;
