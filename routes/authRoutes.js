f8f8d01f7cbdc4038995aef2ba353beee909f5be
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-03-28 05:23:20 +0530

    match and group both

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,24 +2,18 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-// group- by age// country
+// /match-group-by
 
-router.get("/group-by-country", async (req, res) => {
+router.get("/match-group-by", async (req, res) => {
   const collection = mongoose.connection.collection("persons");
 
   try {
     const docs = await collection
       .aggregate([
+        { $match: { favoriteFruit: "banana" } },
         {
-          //"/group-by-age"
-
-          // $group: {
-          //   _id: "$age",
-          // },
-
-          //"/group-by-country"
           $group: {
-            _id: "$company.location.country",
+            _id: { age: "$age", eyeColor: "$eyeColor" },
           },
         },
       ])
@@ -32,41 +26,97 @@ router.get("/group-by-country", async (req, res) => {
   }
 });
 
-// group- by multiple fields
-
-router.get("/group-by-multiple-field", async (req, res) => {
-  const collection = mongoose.connection.collection("persons");
-
-  try {
-    const docs = await collection
-      .aggregate([
-        {
-          $group: {
-            _id: { eyeColorF: "$eyeColor", favoriteFruitF: "$favoriteFruit" },
-          },
-        },
-      ])
-      .toArray();
-    console.log(docs);
-    res.send(docs);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ error: "Error fetching documents" });
-  }
-});
-
-//output
-
-// [
-//   { _id: { eyeColorF: 'green', favoriteFruitF: 'apple' } },
-//   { _id: { eyeColorF: 'green', favoriteFruitF: 'strawberry' } },
-//   { _id: { eyeColorF: 'blue', favoriteFruitF: 'strawberry' } },
-//   { _id: { eyeColorF: 'green', favoriteFruitF: 'banana' } },
-//   { _id: { eyeColorF: 'brown', favoriteFruitF: 'strawberry' } },
-//   { _id: { eyeColorF: 'blue', favoriteFruitF: 'apple' } },
-//   { _id: { eyeColorF: 'brown', favoriteFruitF: 'apple' } },
-//   { _id: { eyeColorF: 'brown', favoriteFruitF: 'banana' } },
-//   { _id: { eyeColorF: 'blue', favoriteFruitF: 'banana' } }
-// ]
 //
+
+// /match-group-by-also-adding-match
+
+router.get("/match-group-by-also-adding-match", async (req, res) => {
+  const collection = mongoose.connection.collection("persons");
+
+  try {
+    const docs = await collection
+      .aggregate([
+        { $match: { gender: "female" } },
+        {
+          $group: {
+            _id: { age: "$age", eyeColor: "$eyeColor", gender: "$gender" },
+          },
+        },
+      ])
+      .toArray();
+    console.log(docs);
+    res.send(docs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: "Error fetching documents" });
+  }
+});
+
+// //output
+// [
+//   { _id: { age: 22, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 33, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 27, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 33, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 25, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 37, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 37, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 22, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 36, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 21, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 25, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 34, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 37, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 31, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 26, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 28, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 34, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 25, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 28, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 24, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 22, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 34, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 20, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 30, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 20, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 35, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 35, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 31, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 31, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 29, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 20, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 32, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 21, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 24, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 30, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 26, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 38, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 39, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 23, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 23, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 23, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 35, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 32, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 28, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 40, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 38, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 30, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 24, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 40, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 29, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 26, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 40, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 27, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 29, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 39, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 38, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 33, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 36, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 27, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 32, eyeColor: 'blue', gender: 'female' } },
+//   { _id: { age: 21, eyeColor: 'green', gender: 'female' } },
+//   { _id: { age: 39, eyeColor: 'brown', gender: 'female' } },
+//   { _id: { age: 36, eyeColor: 'green', gender: 'female' } }
+// ]
+
 module.exports = router;
